74cfc5d097a60638a282476c96fde459330ae377
David Crosby <dcrosby@llnw.com>
2018-12-02 20:25:27 -0500

    notes

--- elwens_animals.love/eventhelpers.lua
+++ elwens_animals.love/eventhelpers.lua
@@ -1,5 +1,8 @@
 local M = {}
 
+-- Iterate event objects, match event.type to eventType.
+-- Handlers is a table whose string keys point to functions.
+-- When an event matches, event.state is used to select the proper func from handlers.
 function M.handle(events, eventType, handlers)
   for _,evt in ipairs(events) do
     if evt.type == eventType then
