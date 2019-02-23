7979d3d197fcbcfee1c97c5e12d132309da953fc
David Crosby <dcrosby@llnw.com>
2019-02-23 11:51:39 -0600

    cleanup code

--- elwens_animals.love/modules/mario/mariosystem.lua
+++ elwens_animals.love/modules/mario/mariosystem.lua
@@ -1,22 +1,28 @@
 local Events = require('eventhelpers')
 
-
-local S = {}
-
-local function handleState_2(evt,mario,e,estore,input,res)
-  local mode = mario.mode
-  if S[mario.mode][evt.action] then S[mario.mode][evt.action](evt,mario,e,estore,input,res) end
-  if mode ~= mario.mode then
-    if S[mode]._exit then S[mode]._exit(evt,mario,e,estore,input,res) end
-    if S[mario.mode]._enter then S[mario.mode]._enter(evt,mario,e,estore,input,res) end
-  end
-  if S[mario.mode]._update then S[mario.mode]._update(evt,mario,e,estore,input,res) end
-end
-
 local JoystickActionMapping = {
   face3="jump",
   face4="dash",
 }
+
+local RunTopSpeed = 400
+local WalkTopSpeed = 280
+local Brake = (0.83/0.016)
+local Accel = 1500
+
+
+local S = {}
+
+local function handleState2(evt,mario,e,estore,input,res)
+  local mode = mario.mode
+  if S[mario.mode][evt.action] then S[mario.mode][evt.action](evt,mario,e,estore,input,res) end
+  -- if mode ~= mario.mode then
+  --   if S[mode]._exit then S[mode]._exit(evt,mario,e,estore,input,res) end
+  --   if S[mario.mode]._enter then S[mario.mode]._enter(evt,mario,e,estore,input,res) end
+  -- end
+  if S[mario.mode]._update then S[mario.mode]._update(evt,mario,e,estore,input,res) end
+end
+
 
 local _event = { action='',value=0 }
 local function translateEvent(evt)
@@ -60,44 +66,12 @@ S.standing = {
 }
 
 S.running = {
-  _update = function(_,mario,e,estore,input,res)
-    e.force.fx = mario.value * 1000 * input.dt
+  _update = function(evt,mario,e,estore,input,res)
   end,
-
-  leftx = function(evt,mario,e,estore,input,res)
-    updateDir(mario,evt)
-    if mario.value == 0 then
-      mario.mode = "standing"
-    end
+  _update = function(evt,mario,e,estore,input,res)
   end,
 
   dash = function(evt,mario,e,estore,input,res)
-    -- if evt.value > 0 then
-    --   mario.dash = true
-    -- else
-    --   mario.dash = false
-    -- end
-  end,
-}
-
-S.jumping = {
-  leftx = function(evt,mario,e,estore,input,res)
-    if evt.value < 0 then
-      mario.facing = "left"
-    elseif evt.value > 0 then
-      mario.facing = "right"
-    else
-      
-    end
-    mario.value = evt.value
-  end,
-
-  dash = function(evt,mario,e,estore,input,res)
-    if evt.value > 0 then
-      mario.dash = true
-    else
-      mario.dash = false
-    end
   end,
 }
 
@@ -119,10 +93,10 @@ local function handleState(evt,mario,e,estore,input,res)
 
 end
 
-local RunTopSpeed = 400
-local WalkTopSpeed = 280
-local Brake = (0.83/0.016)
-local Accel = 1500
+local function brake(vel,dt)
+  vel.dx = dt * Brake * vel.dx
+  if math.abs(vel.dx) < 5 then vel.dx = 0 end
+end
 
 local function update(estore,input,res)
   local events = input.events
@@ -137,60 +111,40 @@ local function update(estore,input,res)
 
     -- Adjust motion
     local maxSpd = WalkTopSpeed
-    local pace = 1
-    -- local pace = 8
     if e.mario.dashbutton then
       maxSpd = RunTopSpeed
-      -- pace = 2
     end
 
     if e.mario.value == 0 then
+      -- not trying to move
       e.force.fx = 0
-      if e.vel.dx ~= 0 then
-        e.vel.dx = input.dt * Brake * e.vel.dx
-        if math.abs(e.vel.dx) < 5 then e.vel.dx = 0 end
-      end
+      brake(e.vel, input.dt)
     else
-      e.force.fx = e.mario.value * Accel
-      if e.vel.dx > maxSpd then
-        e.vel.dx = input.dt * Brake * e.vel.dx
-        -- e.vel.dx = maxSpd
+      -- trying to move
+      if math.abs(e.vel.dx) > maxSpd then
+        -- artificial speed cap
         e.force.fx = 0
-      elseif e.vel.dx < -maxSpd then
-        e.vel.dx = input.dt * Brake * e.vel.dx
-        -- e.vel.dx = -maxSpd
-        e.force.fx = 0
+        brake(e.vel, input.dt)
+      else
+        -- push
+        e.force.fx = e.mario.value * Accel
       end
-    end
-    if e.vel.dx > WalkTopSpeed then
-      pace = 2
     end
 
     -- Update animation state
     local verb = "stand"
-    local pace = 0
     if e.mario.value ~= 0 and e.vel.dx ~= 0 then 
-      if e.mario.dashbutton then
-        -- verb = "run" 
-        verb = "walk" 
-        pace = 2
-      else
-        verb = "walk" 
-        pace = 1
-      end
+      verb = "walk" 
     end
-   
-    e.timers.mario.factor = pace
+    if math.abs(e.vel.dx) > WalkTopSpeed then
+      e.timers.mario.factor = 2
+    else
+      e.timers.mario.factor = 1
+    end
     local animId = "mario_big_"..verb.."_"..e.mario.facing
     if animId ~= e.anim.id then
       e.anim.id = animId
-      -- if verb == "run" then
-      --   e.timers.mario.t = e.timers.mario.t / 2
-      -- elseif verb == "walk" then
-      --   e.timers.mario.t = e.timers.mario.t * 2
-      -- end
     end
-
   end)
 
 end
