2baea052a73f7328b7349b24be3f8a3ad41792b6
David Crosby <dcrosby@llnw.com>
2020-05-09 09:27:04 -0400

    fix accidental reliance on Debug global

--- elwens_animals.love/modules/animalscreen/boundarysystem.lua
+++ elwens_animals.love/modules/animalscreen/boundarysystem.lua
@@ -1,16 +1,18 @@
-require 'ecs.ecshelpers'
-require 'mydebug'
+require "ecs.ecshelpers"
 
-local Debug = Debug.sub("BoundarySystem",false,false)
+local Debug = require("mydebug").sub("BoundarySystem", false, false)
 
 local MaxY = 1000
 local MinY = -1000
 local MaxX = 2000
 local MinX = -1000
 
-return defineUpdateSystem({'pos','vel'}, function(e, estore,input,res)
-  if e.pos.y > MaxY or e.pos.y < MinY or e.pos.x > MaxX or e.pos.x < MinX then
-    estore:destroyEntity(e)
-    Debug.println(e.eid.." fell off the world.")
+return defineUpdateSystem(
+  {"pos", "vel"},
+  function(e, estore, input, res)
+    if e.pos.y > MaxY or e.pos.y < MinY or e.pos.x > MaxX or e.pos.x < MinX then
+      estore:destroyEntity(e)
+      Debug.println(e.eid .. " fell off the world.")
+    end
   end
-end)
+)
--- elwens_animals.love/systems/sound.lua
+++ elwens_animals.love/systems/sound.lua
@@ -1,16 +1,22 @@
-Debug = require 'mydebug'
+local Debug = require("mydebug").sub("Sound", true, true)
 
 -- Accumulate's playtime for "playing" sounds.
 -- For non-looping sounds, once playtime exceeds the duration property, the sound component is deleted.
-return defineUpdateSystem({'sound'},
-  function(e,estore,input,res)
-    for _,sound in pairs(e.sounds) do
-      if sound.state == 'playing' then
+return defineUpdateSystem(
+  {"sound"},
+  function(e, estore, input, res)
+    for _, sound in pairs(e.sounds) do
+      if sound.state == "playing" then
         -- accumulate time for playing sounds
         sound.playtime = sound.playtime + input.dt
+        if (not sound.duration or sound.duration == "") then
+          -- (other systems may need this value, but component originators shouldn't need to dig up the resource info, so we help out here)
+          Debug.println("backfilled duration for " .. tflatten(sound))
+          sound.duration = res.sounds[sound.sound].duration
+        end
         -- check for sound being done:
-        if (not sound.loop) and (sound.duration ~= '') and (sound.playtime > sound.duration) then
-          -- Debug.println("Sound over, removing "..tflatten(sound))
+        if (not sound.loop) and (sound.playtime > sound.duration) then
+          Debug.println("Sound over, removing " .. tflatten(sound))
           e:removeComp(sound)
         end
       end
