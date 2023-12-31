59678368e3e0dcdc3d2f16e156426c0c5b56d9ac
David Crosby <dcrosby42@gmail.com>
2023-12-30 22:20:31 -0500

    farmgame using EcsAdapter

--- elwens_animals.love/modules/farmgame/init.lua
+++ elwens_animals.love/modules/farmgame/init.lua
@@ -1,86 +1,27 @@
--- local Estore = require 'ecs.estore'
 require 'ecs.ecshelpers'
+local EcsAdapter = require 'ecs.moduleadapter'
 local Entities = require 'modules.farmgame.entities'
 local Resources = require 'modules.farmgame.resources'
-local SoundManager = require 'soundmanager'
-local Debug = (require('mydebug')).sub("farmgame.init",true,true,true)
+local DrawSound = require 'systems.drawsound'
 
-local UPDATE = composeSystems(requireModules({
+local UPDATE = composeSystems({
   'systems.timer',
   'systems.physics',
   'systems.sound',
   'systems.touchbutton',
   'modules.farmgame.manipsystem',
   'modules.farmgame.boundarysystem',
-}))
+})
 
-local DRAW = composeDrawSystems(requireModules({
+local DRAW = composeDrawSystems({
+  DrawSound.new("farmgame"),
   'systems.drawstuff',
   'systems.physicsdraw',
-}))
+})
 
-local M = {}
-
-function M.newWorld()
-  local res = Resources.load()
-  local world = {
-    estore = Entities.initialEntities(res),
-    input = {
-      dt = 0,
-      events = {},
-    },
-    resources = res,
-    soundmgr = SoundManager:new(),
-    mute = true,
-  }
-  return world
-end
-
-function M.stopWorld(w)
-  w.soundmgr:clear()
-end
-
-local function resetInput(i)
-  i.dt = 0
-  i.events = {}
-end
-
-function M.updateWorld(w, action)
-  local sidefx = nil
-  if action.type == 'tick' then
-    w.input.dt = action.dt
-    UPDATE(w.estore, w.input, w.resources)
-    sidefx = w.input.events -- return events as potential sidefx
-    resetInput(w.input)
-  elseif action.type == 'touch' then
-    table.insert(w.input.events, shallowclone(action))
-
-  elseif action.type == 'keyboard' and action.state == "pressed" then
-    Debug.println("keyboard action: "..tdebug(action))
-
-  --   -- Random animal creation on keypress
-  --   local x = love.math.random(0, 1024)
-  --   local y = love.math.random(0, 500)
-  --   local kind = pickRandom(w.resources.animalNames)
-  --   local e = Entities.animal(w.estore, w.resources, kind)
-  --   e.pos.x = x
-  --   e.pos.y = y
-  --   Entities.addSound(e, kind, w.resources)
-
-  elseif action.type == 'mouse' then
-    local evt = shallowclone(action)
-    evt.type = "touch"
-    evt.id = 1
-    table.insert(w.input.events, evt)
-  end
-  return w, sidefx
-end
-
-function M.drawWorld(w)
-  if not w.mute then
-    w.soundmgr:update(w.estore, nil, w.resources)
-  end
-  DRAW(w.estore, w.resources)
-end
-
-return M
+return EcsAdapter({
+  loadResources=Resources.load,
+  create=Entities.initialEntities,
+  update=UPDATE,
+  draw=DRAW,
+})
