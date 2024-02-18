6c1683377d5f9722e3a7741ff959a64f5ef34719
David Crosby <dcrosby42@gmail.com>
2024-02-18 13:06:55 -0500

    Upgrade player_control_system

--- elwens_animals.love/modules/sungirl/player_control_system.lua
+++ elwens_animals.love/modules/sungirl/player_control_system.lua
@@ -1,20 +1,38 @@
 local EventHelpers = require 'eventhelpers'
 local Debug = require('mydebug').sub("PlayerControl")
 
+local function mapKey(key)
+  if key == "space" then
+    return "action"
+  end
+  -- certain keys will just match up, like "left" "down" etc.
+  return key
+end
+
 return defineUpdateSystem(
   allOf(hasTag('player'), hasComps('player_control')),
   function(e, estore, input, res)
     local con = e.player_control
+    con.just_pressed = {}
+    con.just_released = {}
     EventHelpers.handle(input.events, 'keyboard', {
       pressed = function(event)
-        if con[event.key] ~= nil then
-          con[event.key] = true
+        local key = mapKey(event.key)
+        if con[key] ~= nil then
+          if con[key] ~= true then
+            con.just_pressed[key] = true
+          end
+          con[key] = true
         end
       end,
 
       released = function(event)
-        if con[event.key] ~= nil then
-          con[event.key] = false
+        local key = mapKey(event.key)
+        if con[key] ~= nil then
+          if con[key] == true then
+            con.just_released[key] = true
+          end
+          con[key] = false
         end
       end,
     })
